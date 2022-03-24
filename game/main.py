e8fe486e2158bfe66a0ca474d4356b933400384f
konmin123 <MindlinKS@yandex.ru>
2022-03-24 16:05:09 +0300

    TicTacToe

--- game/main.py
+++ game/main.py
@@ -10,7 +10,7 @@ class Field:
     def __init__(self, size: int = 3):
         """ Конструктор поля. """
         self.size = size  # prop
-        self.__field: list[list[Optional[int]]] = [[None for _ in range(size)] for _ in range(size)]
+        self.__field: list[list[Optional[str]]] = [[None for _ in range(size)] for _ in range(size)]
 
     def print(self):
         """ Метод вывода на экран игрового поля. """
@@ -18,48 +18,47 @@ class Field:
             for cell in line:
                 if not cell:
                     print("[ ]", end="")
-                if cell == 1:
-                    print(f"[X]", end="")
-                if cell == 2:
-                    print(f"[O]", end="")
+                else:
+                    print(f"[{cell}]", end="")
             print()
         print("-" * self.size*3)
 
     def is_filled(self, line: int, cell: int) -> bool:
         return bool(self.__field[line][cell])
 
-    def fill_cell(self, player_number: int, line: int, cell: int) -> bool:
+    def fill_cell(self, player_symbol: str, line: int, cell: int) -> bool:
         """ Ход игрока в ячейку поля """
-        self.__field[line][cell] = player_number
-        return self.__check_win(player_number, line, cell)
+        self.__field[line][cell] = player_symbol
+        return self.__check_win(player_symbol, line, cell)
 
-    def __check_win(self, player: int, line: int, cell: int) -> bool:
+    def __check_win(self, player_symbol: str, line: int, cell: int) -> bool:
         #  горизонтальная проверка
         for cell_ in range(self.size):
-            if not self.__field[line][cell_] == player:
+            if not self.__field[line][cell_] == player_symbol:
                 break
         else:
             return True
 
         #  вертикальная проверка
         for line_ in range(self.size):
-            if not self.__field[line_][cell] == player:
+            if not self.__field[line_][cell] == player_symbol:
                 break
         else:
             return True
 
         #  одновременная проверка диагоналей
         for index in range(self.size):
-            if not self.__field[index][index] == player:
-                if not self.__field[index][self.size - 1 - index] == player:
+            if not self.__field[index][index] == player_symbol:
+                if not self.__field[index][self.size - 1 - index] == player_symbol:
                     return False
         else:
             return True
 
 
 class Player:
-    def __init__(self, number: int):
+    def __init__(self, number: int, symbol: str):
         self.number = number
+        self.symbol = symbol
 
 
 class TicTacToe:
@@ -74,8 +73,8 @@ class TicTacToe:
     def start(self):
         print("Приветствую Вас в игре TicTacToe!")
 
-        player1 = Player(1)
-        player2 = Player(2)
+        player1 = Player(1, 'X')
+        player2 = Player(2, '0')
 
         field = Field(int(input("Введите размер поля: ")))
 
@@ -84,7 +83,7 @@ class TicTacToe:
             print(f"ходит игрок{player_.number}")
 
             line, cell = self.__input_cell(field)
-            if field.fill_cell(player_.number, line, cell):
+            if field.fill_cell(player_.symbol, line, cell):
                 field.print()
                 print(f"Победил игрок {player_.number}, Поздравляю!")
                 return
